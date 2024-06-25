8d06e591c4de519ce6fa3e063f87d764ef63a9ff
Pratik Tandel <pratik.tandel@1rivet.comgit>
2024-06-25 17:39:18 +0530

    feat: customize tab navbar

--- App.tsx
+++ App.tsx
@@ -1,38 +1,65 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable prettier/prettier */
-import React, { useEffect } from 'react';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import React, { useEffect } from 'react';
 import Signin from './src/screens/auth/Signin';
 // import Config from 'react-native-config';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Image } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Favorites from './src/screens/app/Favorites';
+import Home from './src/screens/app/Home';
+import Profile from './src/screens/app/Profile';
 import Signup from './src/screens/auth/Signup';
 import Splash from './src/screens/auth/Splash';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './src/screens/app/Home';
-import Favorites from './src/screens/app/Favorites';
-import Profile from './src/screens/app/Profile';
-import Settings from './src/screens/app/Settings';
+import { colors } from './src/utils/colors';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused }) => {
+        let icon;
+
+        if (route.name === 'Home') {
+          icon = focused
+            ? require('./src/assets/tabs/home_active.png')
+            : require('./src/assets/tabs/home.png');
+        } else if (route.name === 'Profile') {
+          icon = focused
+            ? require('./src/assets/tabs/profile_active.png')
+            : require('./src/assets/tabs/profile.png');
+        } else if (route.name === 'Favorites') {
+          icon = focused
+            ? require('./src/assets/tabs/bookmark_active.png')
+            : require('./src/assets/tabs/bookmark.png');
+        }
+
+        // You can return any component that you like here!
+        return <Image style={{ width: 24, height: 24 }} source={icon} />;
+      },
+      headerShown: false,
+      tabBarShowLabel: false,
+      tabBarStyle: { borderTopColor: colors.lightGrey }
+    })}
+  >
     <Tab.Screen name="Home" component={Home} />
     <Tab.Screen name="Favorites" component={Favorites} />
     <Tab.Screen name="Profile" component={Profile} />
-    <Tab.Screen name="Settings" component={Settings} />
   </Tab.Navigator>
 );
 
 function App(): React.JSX.Element {
-  const isSignedIn = false;
+  const isSignedIn = true;
 
   useEffect(() => {
     GoogleSignin.configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-      webClientId: 'web client id here', // client ID of type WEB for your server. Required to get the idToken on the user object, and for offline access.
+      webClientId: '524318510372-ncktjfvvohr7n2vqqaoa09fn9por3164.apps.googleusercontent.com', // client ID of type WEB for your server. Required to get the idToken on the user object, and for offline access.
       offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
       // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
     });
